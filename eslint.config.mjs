34186b183edbabdb19a81bf635dc87d8f3d2f207
Jordan Boesch <59945426+boeschj@users.noreply.github.com>
2024-12-17 21:15:58 -0600

    feat: configure linter and formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,63 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import jsxAccessibilityPlugin from 'eslint-plugin-jsx-a11y';
+import tseslint from 'typescript-eslint';
+import tailwindPlugin from 'eslint-plugin-tailwindcss';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: {
+            next: nextPlugin,
+            react: reactPlugin,
+            'react-hooks': reactHooksPlugin,
+            'jsx-a11y': jsxAccessibilityPlugin,
+            tailwindcss: tailwindPlugin,
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+            '@typescript-eslint/restrict-template-expressions': [
+                'error',
+                {
+                    allowNumber: true,
+                    allowBoolean: true,
+                    allowAny: false,
+                    allowNullish: true,
+                },
+            ],
+        },
+        languageOptions: {
+            parserOptions: {
+                project: true,
+            },
+        },
+    },
+    {
+        files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
+        ...tseslint.configs.disableTypeChecked,
+    },
+    {
+        ignores: [
+            '.eslintrc.js',
+            '.eslintrc.mjs',
+            '.prettierrc.js',
+            '.prettierrc.mjs',
+            'node_modules/**',
+            'dist/**',
+            'build/**',
+            '.next/**',
+            '**/*.config.js',
+            '**/*.config.mjs',
+            'eslint.config.mjs'
+        ],
+    }
+);
